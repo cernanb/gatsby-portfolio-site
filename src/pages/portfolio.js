4b348a80e3ac8552c7bd00e8a9f9dfa541673b61
Cernan Bernardo <cernanb@gmail.com>
2018-04-11 20:26:55 -0700

    Start portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,11 +1,74 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import '../layouts/portfolio/Projects-Horizontal.css'
+import goodEats from './images/good-eats.png'
 
 const Portfolio = () => (
   <div>
-    <h1>Here is my portfolio</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1 className="text-center">My Work</h1>
+    <div className="projects-horizontal">
+      <div className="container">
+        <div className="intro">
+          <p className="text-center">See my projects below</p>
+        </div>
+        <div className="row projects">
+          <div className="col-sm-12 item">
+            <div className="row">
+              <div className="col-md-12 col-lg-6">
+                <a href="#">
+                  <img className="img-fluid" src={`${goodEats}`} />
+                </a>
+              </div>
+              <div className="col" style={{ textAlign: 'center' }}>
+                <h1 style={{ fontSize: '24px' }} className="name">
+                  Project Name
+                </h1>
+                <p className="description">
+                  Aenean tortor est, vulputate quis leo in, vehicula rhoncus
+                  lacus. Praesent aliquam in tellus eu gravida.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 item">
+            <div className="row">
+              <div className="col" style={{ textAlign: 'center' }}>
+                <h1 style={{ fontSize: '24px' }} className="name">
+                  Project Name
+                </h1>
+                <p className="description">
+                  Aenean tortor est, vulputate quis leo in, vehicula rhoncus
+                  lacus. Praesent aliquam in tellus eu gravida.
+                </p>
+              </div>
+              <div className="col-md-12 col-lg-6">
+                <a href="#">
+                  <img className="img-fluid" src={`${goodEats}`} />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 item">
+            <div className="row">
+              <div className="col-md-12 col-lg-6">
+                <a href="#">
+                  <img className="img-fluid" src={`${goodEats}`} />
+                </a>
+              </div>
+              <div className="col" style={{ textAlign: 'center' }}>
+                <h1 style={{ fontSize: '24px' }} className="name">
+                  Project Name
+                </h1>
+                <p className="description">
+                  Aenean tortor est, vulputate quis leo in, vehicula rhoncus
+                  lacus. Praesent aliquam in tellus eu gravida.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
